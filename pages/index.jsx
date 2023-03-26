1b53878a0715c18e46a3c6c476cd16b9908c772d
JozzaOzza <JamieOrr13@gmail.com>
2023-03-26 21:29:08 +0100

    text, submit + clear buttons, conditional response

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,23 +5,41 @@ function Header({ title }) {
 
 export default function HomePage() {
   const names = ['Jamie', 'Orr', 'Project'];
-
-  const [likes, setLikes] = useState(0);
-
-  function handleClick() {
-    setLikes(likes + 1);
+  const [word, setWord] = useState('');
+  const [result, setResult] = useState('')
+  
+  function textTyping(event) {
+    setWord(event.target.value);
+  }
+  function wordSubmit() {
+    setResult(word.toLowerCase() == "ciao" ?
+    "Congratulations, you were correct" :
+    "Not quite, try again :)");
+  }
+  function clearText() {
+    setWord("");
+    setResult("");
   }
 
+
   return (
+    
     <div>
-      <Header title="Develop. Preview. Ship. 🚀" />
+      <Header title="My Italian Website Project" />
       <ul>
         {names.map((name) => (
           <li key={name}>{name}</li>
         ))}
       </ul>
+      
 
-      <button onClick={handleClick}>Like ({likes})</button>
+      <div>Type out the Italian for 'Hello', and submit your answer</div> <br />  
+      <input type="text" id="hello" name="hello" onChange={textTyping} value={word} autoComplete="off"></input > <button onClick={clearText}>Clear</button> <br /><br /> 
+      <button onClick={wordSubmit}>Click to submit your answer</button> <br /> <br />
+      <div>{result}</div>
+
+      <br></br><br></br>
     </div>
+
   );
 }
