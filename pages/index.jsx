7d6f59590b8fa43b4953d8a522531850f48bca8f
JozzaOzza <JamieOrr13@gmail.com>
2023-03-27 23:06:00 +0100

    list of words, questions asked randomly

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,47 +1,57 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
 function Header({ title }) {
   return <h1>{title ? title : 'Default title'}</h1>;
 }
 
+const wordList = [["hello", "ciao"], ["however", "ciononostante"], ["but", "ma"], ["and", "e"], ["after", "dopo"]]
+
 export default function HomePage() {
   
   // states
-  const names = ['Jamie', 'Orr', 'Project'];
-  const [word, setWord] = useState('');
+
+  const [answer, setAnswer] = useState('')
+  const [question, setQuestion] = useState(wordList[0])
   const [result, setResult] = useState('')
   
   // functions
-  function textTyping(event) {
-    setWord(event.target.value);
+
+  function updateAnswer(event) {
+    setAnswer(event.target.value)
   }
-  function wordSubmit() {
-    setResult(word.toLowerCase() == "ciao" ?
+
+  function submitAnswer() {
+    setResult(answer.toLowerCase() == question[1] ?
     "Congratulations, you were correct" :
     "Not quite, try again :)");
   }
+
   function clearText() {
-    setWord("");
+    setAnswer("")
     setResult("");
   }
 
-  // html
+  function newQuestion() {
+    clearText()
+    setQuestion(wordList[Math.floor(Math.random() * wordList.length)])
+  }
+
+  // jsx
+
   return (
     
     <div>
       
       <Header title="My Italian Website Project" />
       
-      <ul>
-        {names.map((name) => (
-          <li key={name}>{name}</li>
-        ))}
-      </ul>
+      <div>The aim of this project is to create a website where you can practice vocab learning in a foreign language</div> <br />
       
-      <div>Type out the Italian for 'Hello', and submit your answer</div> <br />  
-      <input type="text" id="hello" name="hello" onChange={textTyping} value={word} autoComplete="off"></input > 
+      <div>Type out the word in Italian which means "{question[0]}"</div> <br />  
+      <input type="text" id="attempt" onChange={updateAnswer} value={answer} autoComplete="off"></input > 
       <button onClick={clearText}>Clear</button> <br /><br /> 
-      <button onClick={wordSubmit}>Click to submit your answer</button> <br /> <br />
+      <button onClick={submitAnswer}>Click to submit your answer</button> <br /> <br />
       <div>{result}</div> <br />
+      <button onClick={newQuestion}>Click to receive a new word</button>
 
     </div>
 
