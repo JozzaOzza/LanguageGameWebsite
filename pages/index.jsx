2acac3c4ed6c6b99ecc2e150b59f1e39cd7ececc
JozzaOzza <JamieOrr13@gmail.com>
2023-03-30 21:37:28 +0200

    minor fiddles

--- pages/index.jsx
+++ pages/index.jsx
@@ -36,7 +36,7 @@ export default function HomePage() {
     setQuestion(wordList[Math.floor(Math.random() * wordList.length)])
   }
 
-  // jsx
+  // html
 
   return (
     
@@ -46,12 +46,13 @@ export default function HomePage() {
       
       <div>The aim of this project is to create a website where you can practice vocab learning in a foreign language</div> <br />
       <div>Currently, the only supported language is Italian</div> <br />
+      <div>Once we've figured out one langauge, then maybe we can think about extending to more</div> <br /> <br />
       
       <div>Type out the word in Italian which means "{question[0]}"</div> <br />  
       <input type="text" id="attempt" onChange={updateAnswer} value={answer} autoComplete="off"></input > 
       <button onClick={clearText}>Clear</button> <br /><br /> 
       <button onClick={submitAnswer}>Click to submit your answer</button> <br /> <br />
-      <div>{result}</div> <br />
+      <div >{result}</div> <br />
       <button onClick={newQuestion}>Click to receive a new word</button>
 
     </div>
