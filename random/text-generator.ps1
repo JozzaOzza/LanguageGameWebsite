8597e6365f05f1be42294af4dee404ccbb5a18c0
JozzaOzza <JamieOrr13@gmail.com>
2023-05-14 10:43:45 +0100

    can convert text from Ita500 easier

---
+++ random/text-generator.ps1
@@ -0,0 +1,38 @@
+# function which takes line and manipulates it
+
+$pattern = '[^ a-zA-Z]'
+function EditString {
+    param (
+        [string]$line
+    )
+    # Extract the words
+    $words = $line -split ' – '
+
+    # Remove slash from Italian side
+    $slashIndex = $words[0].IndexOf('/')
+    $word = If ($slashIndex -gt -1) {$words[0].Substring(0, $slashIndex)} Else {$words[0]}
+    
+    # Remove speech marks and 'or' from English side
+    $translations = $words[1] -replace $pattern, '' -split ' or '
+    $translationsString = $translations -join ', '
+
+    # Build the desired output
+    $output = "$translationsString = $word"
+
+    return $output
+}
+
+function RemoveNonAlphabeticCharacters {
+    param (
+        [string]$word
+    )
+    $word -replace $pattern, ''
+
+}
+
+Clear-Content .\text-generator-output.txt
+Get-Content '.\block-text.txt' | ForEach-Object {
+    $editedLine = EditString $_
+    Write-Host $editedLine 
+    Add-Content -Path .\text-generator-output.txt -Value $editedLine
+}
