a34f512f7216e2d40d18cc45b7c1e635b42e183e
Jamie Orr <Jamie.Orr@newday.co.uk>
2023-05-20 18:35:46 +0100

    cleaned up home page

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -13,41 +13,6 @@ export default function HomePage() {
   
   // states
 
-  const [words, setWords] = useState([[]])
-  const [hide, setHide] = useState(true)
-
-  useEffect(() => {
-    console.log(`First word in list is: ${words[0][0]}`);
-  }, [words])
-
-  // functions
-
-  function getData(topic) {
-    fetch(`http://localhost:5000/api/${topic}`).then(
-      Response => Response.json()
-    ).then(
-      Data => {
-        console.log(Data.words)
-        setWords(Data.words)
-      }
-    )
-  }
-
-  function callDbScript(topic) {
-    fetch(`http://localhost:5000/api/db`).then(
-      Response => Response.json()
-    ).then(
-      Data => {
-        console.log(Data.words)
-      }
-    )
-  }
-
-  function startSession(topic) {
-    getData(topic)
-    setHide(false)
-  }
-
   // html
 
   return (
@@ -61,23 +26,7 @@ export default function HomePage() {
       <div>Words are taken from 
         {" "}  
          <a href="https://www.fluentin3months.com/italian-words/#h-the-100-most-used-italian-nouns-20-more-nouns-you-need-to-know">this website</a>
-      </div> <br />
-      <button id='nounsButton' onClick={() => {
-          document.getElementById("nounsButton").style.display="none"
-          document.getElementById("verbsButton").style.display="none"
-          startSession("nouns")
-        }}>Nouns</button> 
-      {" "}
-      <button id='verbsButton' onClick={() => {
-          document.getElementById("nounsButton").style.display="none"
-          document.getElementById("verbsButton").style.display="none"
-          startSession("verbs")
-        }}>Verbs</button>
-      {" "}
-      <button id='dbButton' onClick={() => {
-          callDbScript()
-        }}>Data</button> <br /> <br />  
-      <>{!hide && <Session id='session' words={words} style=''/>}</>  
+      </div> <br /> <br />
 
     </div>
 
