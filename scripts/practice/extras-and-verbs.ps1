a8518ed9b3cb354ee99f9fd2e156d92c533b34a5
Jamie Orr <Jamie.Orr@newday.co.uk>
2025-09-05 12:24:24 +0100

    new script and proper ser

---
+++ scripts/practice/extras-and-verbs.ps1
@@ -0,0 +1,19 @@
+using namespace System.Collections.Generic
+
+# & ".\extras-and-verbs.ps1"
+[CmdletBinding()]
+param ()  
+process {
+    $topicNumberedList = @("`n(1) Extras`n(2) Verbs")
+
+    # Get input from user, either verbs or extras
+    $topicResponse = Read-Host ("Pick a topic. The options are:{0}`n" -f $topicNumberedList)
+    $topicResponseNumber = ([int]$topicResponse) - 1
+
+    if ($topicResponseNumber -eq 0) {
+        & ".\extras-tester-choice.ps1"
+    }
+    else {
+        & ".\verb-tester.ps1" -AllConjugations
+    }
+}
